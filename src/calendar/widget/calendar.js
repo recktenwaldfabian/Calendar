f6d5460168d480811b6028f037676ebc7cb4584e
recktenwaldfabian <fabian.recktenwald@mansystems.de>
2018-11-15 13:33:49 +0100

    Fix Issue - event create reference
    * adjust logic for setting references (old code set the calender reference to a resource object)

--- src/calendar/widget/calendar.js
+++ src/calendar/widget/calendar.js
@@ -478,10 +478,10 @@ define([
                     entity: this.eventEntity,
                     callback: function(obj) {
                         this._setVariables(obj, eventData, this.startAttr, this.endAttr, allDay);
-                        this._setResourceReference(obj, this.neweventref, jsEvent.resourceId, this._mxObj);
-                        if ((resource || this._mxObj) && this.neweventref !== "") {
-                            obj.addReference(this.neweventref.split("/")[0], (resource ? resource.id : this._mxObj.getGuid()));
-                        }
+                        this._setResourceReference(obj, this.neweventref, null, this._mxObj);
+                        this._setResourceReference(obj, this.resourceEventPath, jsEvent.resourceId, null);
+                        this._setResourceReference(obj, this.resourceEventPath, resource.id, null);
+
                         this._execMF(obj, this.neweventmf);
                     },
                     error: function(err) {
